f146336a0b16dc790d5d57ca424b0c6d8d9880cb
Foxies777 <gaazovalmaz589@gmail.com>
2024-06-07 01:40:38 +0300

    async routing

--- src/components/AppRouter.tsx
+++ src/components/AppRouter.tsx
@@ -1,22 +1,30 @@
 import { Routes, Route, Navigate } from 'react-router-dom';
-import { publicRoutes, authRoutes } from '../../routes';
+import { useUnit } from 'effector-react';
+import { publicRoutes, authRoutes } from '../routes';
+import { fetchUserFx, $user } from '../shared/auth';
+import { useEffect } from 'react';
 
 const AppRouter = () => {
-  const isAuth = localStorage.getItem('email') || null;
+  const user = useUnit($user);
+  const fetchUser = useUnit(fetchUserFx);
+
+  useEffect(() => {
+    fetchUser();
+  }, [fetchUser]);
+
   return (
     <Routes>
-      {isAuth ? authRoutes.map(({ path, Component }) => (
+      {user ? authRoutes.map(({ path, Component }) => (
         <Route key={path} path={path} element={<Component />} />
-
       )) : publicRoutes.map(({ path, Component }) => (
         <Route key={path} path={path} element={<Component />} />
       ))}
-      {isAuth ?
+      {user ?
         (<Route path='*' element={<Navigate to='/profile' />} />)
         :
         (<Route path='*' element={<Navigate to='/' />} />)}
     </Routes>
-  )
-}
+  );
+};
 
-export default AppRouter
+export default AppRouter;
--- src/shared/auth/index.ts
+++ src/shared/auth/index.ts
@@ -1,10 +1,12 @@
-import { createEvent, createStore } from 'effector';
+import { createEvent, createStore, createEffect, sample } from 'effector';
 import { persist } from 'effector-storage/local';
+import { signIn } from '../../shared/api/auth';
+import { showErrorMessageFx } from '../../shared/notification';
 
 // Define user type
 interface User {
-    email: string;
-    password: string;
+  email: string;
+  password: string;
 }
 
 // Create stores and events for handling users
@@ -12,27 +14,56 @@ const $users = createStore<User[]>([]);
 export const addUser = createEvent<User>();
 export const removeUser = createEvent<void>();
 
-const $user = createStore<User | null>(null);
+export const $user = createStore<User | null>(null);
+export const fetchUserFx = createEffect(async (): Promise<User | null> => {
+  const userString = localStorage.getItem('user');
+  if (userString) {
+    return JSON.parse(userString) as User;
+  }
+  return null;
+});
 
 $user
-    .on(addUser, (_, user) => user)
-    .reset(removeUser);
+  .on(addUser, (_, user) => user)
+  .reset(removeUser)
+  .on(fetchUserFx.doneData, (_, user) => user);
 
 $users.on(addUser, (state, user) => [...state, user]);
-persist({
-    key: 'users',
-    store: $users,
-    serialize: (users) => JSON.stringify(users),
-    deserialize: (value) => {
-        const parsed = JSON.parse(value);
-        return Array.isArray(parsed) ? parsed : [];
-    },
-})
 
 persist({
-    key: 'user',
-    store: $user,
-    serialize: (value) => JSON.stringify(value),
-    deserialize: (value) => JSON.parse(value)
+  key: 'users',
+  store: $users,
+  serialize: (users) => JSON.stringify(users),
+  deserialize: (value) => {
+    const parsed = JSON.parse(value);
+    return Array.isArray(parsed) ? parsed : [];
+  },
 });
-// localStorage.clear();
+
+persist({
+  key: 'user',
+  store: $user,
+  serialize: (value) => JSON.stringify(value),
+  deserialize: (value) => {
+    return value ? JSON.parse(value) : null;
+  },
+});
+
+export const signInFx = createEffect(signIn);
+
+sample({
+  clock: signInFx.doneData,
+  fn: (response) => {
+    console.log(response);
+    return {
+      email: response.email,
+      password: response.password,
+    };
+  },
+  target: addUser,
+});
+
+sample({
+  clock: signInFx.failData,
+  target: showErrorMessageFx,
+});
